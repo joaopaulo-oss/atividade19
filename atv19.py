1906b6a4b9b6d680fb0e9c94865b6f7c46616346
joaopaulo-oss <joaopaulo24973@gmail.com>
2024-09-17 09:21:12 -0300

    fatorial

---
+++ atv19.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba um número inteiro e calcule o fatorial desse número usando uma estrutura de repetição.
+n = 1
+n = int(input("Digite um numero inteiro: "))
+while n >= 0:
+    fatorial = 1
+    while n > 1:
+        fatorial = fatorial * n
+    n = n - 1
+    print(fatorial)
+ 
+  
